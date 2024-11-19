77e8fdee8e23e3bb232aa5cc3b87eeda5cf0f7b4
Deepak <deepakworkpc@gmail.com>
2024-11-20 01:34:53 +0530

    feat: disable geolocation

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(async (position) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(async (position) => {
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
 
-        const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&forecast_days=1`);
-        const record = await res.json();
-        document.querySelector(".temp-of-place").innerHTML = record.current.temperature_2m;
+//         const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&forecast_days=1`);
+//         const record = await res.json();
+//         document.querySelector(".temp-of-place").innerHTML = record.current.temperature_2m;
     
-        // console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-    });
-} else {
-    console.log("Geolocation is not supported by this browser.");
-}
-
-
-// async function fetchData() {
-//     const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=22.3052&longitude=73.1972&current=temperature_2m&forecast_days=1");
-//     const record = await res.json();
-//     document.querySelector(".temp-of-place").innerHTML = record.current.temperature_2m;
-//     // document.getElementById("areaName").innerHTML=record.data[0].areaName;
-//     // document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-//     // document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+//         // console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+//     });
+// } else {
+//     console.log("Geolocation is not supported by this browser.");
 // }
-// fetchData();
+
+
+async function fetchData() {
+    const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=22.3052&longitude=73.1972&current=temperature_2m&forecast_days=1");
+    const record = await res.json();
+    document.querySelector(".temp-of-place").innerHTML = record.current.temperature_2m;
+    // document.getElementById("areaName").innerHTML=record.data[0].areaName;
+    // document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+    // document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+}
+fetchData();
