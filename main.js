ff89398d374a163282ad07949178f44a650038cf
Deepak <deepakworkpc@gmail.com>
2024-11-20 01:35:49 +0530

    refactor: clean up the unused variables

--- main.js
+++ main.js
@@ -17,9 +17,6 @@
 async function fetchData() {
     const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=22.3052&longitude=73.1972&current=temperature_2m&forecast_days=1");
     const record = await res.json();
-    document.querySelector(".temp-of-place").innerHTML = record.current.temperature_2m;
-    // document.getElementById("areaName").innerHTML=record.data[0].areaName;
-    // document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-    // document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+    document.querySelector(".temp-of-place").textContent = record.current.temperature_2m;
 }
 fetchData();
